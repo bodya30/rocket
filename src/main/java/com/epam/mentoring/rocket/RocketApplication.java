a69497ee49652e84d56a3f5e7d4e5ded4dafa3a4
Bohdan Tkach <tkachulo@gmail.com>
2018-12-01 16:07:43 +0200

    initial commit

---
+++ src/main/java/com/epam/mentoring/rocket/RocketApplication.java
@@ -0,0 +1,12 @@
+package com.epam.mentoring.rocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketApplication.class, args);
+	}
+}
